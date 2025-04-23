5a418c99c9d19d38b5c3ef3123ec1664581b619c
Rob Blackbourn <rob.blackbourn@gmail.com>
2025-04-23 19:59:23 +0100

    Reworked

--- scratch/example_sqlite3_v1.py
+++ scratch/example_sqlite3_v1.py
@@ -21,7 +21,8 @@ def ensure_database(database: str | Path) -> str | Path:
         database = Path(database)
 
     if database.exists():
-        raise FileExistsError(f"Database exists: {database}")
+        database.unlink()
+        # raise FileExistsError(f"Database exists: {database}")
 
     if not database.parent.exists():
         database.parent.mkdir()
