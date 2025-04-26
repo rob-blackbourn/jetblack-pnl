4ac44c61e2422c358f4cf935874f9289a711812b
Rob Blackbourn <rob.blackbourn@gmail.com>
2025-04-26 13:09:17 +0100

    simplified

--- scratch/example_sqlite3_v2.py
+++ scratch/example_sqlite3_v2.py
@@ -1,13 +1,16 @@
 """SQLite example"""
 
+from contextlib import contextmanager
 from datetime import datetime, timedelta
 from decimal import Decimal
 from pathlib import Path
 import sqlite3
+from typing import Any, Generator
 
 from jetblack_pnl.impl.sqlite3_v2 import (
-    Trade,
+    create_tables,
     register_handlers,
+    Trade,
     Security,
     Book,
     DbPnlBook
@@ -30,60 +33,76 @@ def ensure_database(database: str | Path) -> str | Path:
     return database
 
 
+@contextmanager
+def cursor(con: sqlite3.Connection) -> Generator[sqlite3.Cursor, None, None]:
+    cur = con.cursor()
+    try:
+        yield cur
+    except:
+        con.rollback()
+        raise
+    finally:
+        cur.close()
+
+
 def main(database: str | Path):
     register_handlers()
 
     database = ensure_database(database)
 
-    con = sqlite3.connect(database, detect_types=sqlite3.PARSE_DECLTYPES)
-    pnl_book = DbPnlBook(con)
+    with sqlite3.connect(database, detect_types=sqlite3.PARSE_DECLTYPES) as con:
+        pnl_book = DbPnlBook()
 
-    # book.drop()
-    pnl_book.create_tables()
+        # book.drop()
+        create_tables(con.cursor())
 
-    apple = Security.create(con, 'AAPL', Decimal(1), False)
-    tech = Book.create(con, 'tech')
+        apple = Security.create(con, 'AAPL', Decimal(1), False)
+        tech = Book.create(con, 'tech')
 
-    # Buy 6 @ 100
-    ts = datetime(2000, 1, 1, 9, 0, 0, 0)
-    trade = Trade.create(con, ts, apple, tech, 6, 100)
-    pnl = pnl_book.add(trade)
-    print(pnl)
+        # Buy 6 @ 100
+        ts = datetime(2000, 1, 1, 9, 0, 0, 0)
+        trade = Trade.create(con, ts, apple, tech, 6, 100)
+        with cursor(con) as cur:
+            pnl = pnl_book.add_trade(apple, tech, trade, cur)
+        print(pnl)
 
-    # Buy 6 @ 106
-    ts += timedelta(seconds=1)
-    trade = Trade.create(con, ts, apple, tech, 6, 106)
-    pnl = pnl_book.add(trade)
-    print(pnl)
+        # Buy 6 @ 106
+        ts += timedelta(seconds=1)
+        trade = Trade.create(con, ts, apple, tech, 6, 106)
+        with cursor(con) as cur:
+            pnl = pnl_book.add_trade(apple, tech, trade, cur)
+        print(pnl)
 
-    # Buy 6 @ 103
-    ts += timedelta(seconds=1)
-    trade = Trade.create(con, ts, apple, tech, 6, 103)
-    pnl = pnl_book.add(trade)
-    print(pnl)
+        # Buy 6 @ 103
+        ts += timedelta(seconds=1)
+        trade = Trade.create(con, ts, apple, tech, 6, 103)
+        with cursor(con) as cur:
+            pnl = pnl_book.add_trade(apple, tech, trade, cur)
+        print(pnl)
 
-    # Sell 9 @ 105
-    ts += timedelta(seconds=1)
-    trade = Trade.create(con, ts, apple, tech, -9, 105)
-    pnl = pnl_book.add(trade)
-    print(pnl)
+        # Sell 9 @ 105
+        ts += timedelta(seconds=1)
+        trade = Trade.create(con, ts, apple, tech, -9, 105)
+        with cursor(con) as cur:
+            pnl = pnl_book.add_trade(apple, tech, trade, cur)
+        print(pnl)
 
-    # Sell 12 @ 107
-    ts += timedelta(seconds=1)
-    trade = Trade.create(con, ts, apple, tech, -12, 107)
-    pnl = pnl_book.add(trade)
-    print(pnl)
+        # Sell 12 @ 107
+        ts += timedelta(seconds=1)
+        trade = Trade.create(con, ts, apple, tech, -12, 107)
+        with cursor(con) as cur:
+            pnl = pnl_book.add_trade(apple, tech, trade, cur)
+        print(pnl)
 
-    # Buy 3 @ 103
-    ts += timedelta(seconds=1)
-    trade = Trade.create(con, ts, apple, tech, 3, 103)
-    pnl = pnl_book.add(trade)
-    print(pnl)
-
-    con.close()
+        # Buy 3 @ 103
+        ts += timedelta(seconds=1)
+        trade = Trade.create(con, ts, apple, tech, 3, 103)
+        with cursor(con) as cur:
+            pnl = pnl_book.add_trade(apple, tech, trade, cur)
+        print(pnl)
 
 
 if __name__ == '__main__':
-    # DATABASE = ":memory:"
-    DATABASE = "tmp/pnl.sqlite"
+    DATABASE = ":memory:"
+    # DATABASE = "tmp/pnl.sqlite"
     main(DATABASE)
--- src/jetblack_pnl/impl/sqlite3_v2/pnl_book.py
+++ src/jetblack_pnl/impl/sqlite3_v2/pnl_book.py
@@ -1,6 +1,6 @@
 """A basic database implementation"""
 
-from sqlite3 import Connection, Cursor
+from sqlite3 import Cursor
 
 from ...core import TradingPnl, PnlBook
 
@@ -9,41 +9,15 @@ from .matched_pool import MatchedPool
 from .security import Security
 from .trade import Trade
 from .unmatched_pools import UnmatchedPool
-from .tables import create_tables, drop_tables
 from .pnl_book_store import PnlBookStore
 
 
 class DbPnlBook(PnlBook[Security, Book, Trade, Cursor]):
 
-    def __init__(self, con: Connection) -> None:
-        self._con = con
+    def __init__(self) -> None:
         super().__init__(
             PnlBookStore(),
             lambda security, book, context: MatchedPool(security, book),
             lambda security, book, context: UnmatchedPool.Fifo(security, book),
         )
         self._pnl: dict[tuple[int, int], TradingPnl] = {}
-
-    def add(
-        self,
-        trade: Trade,
-    ) -> TradingPnl:
-        cur = self._con.cursor()
-        pnl = super().add_trade(trade.security, trade.book, trade, cur)
-        self._con.commit()
-        cur.close()
-        return pnl
-
-    def create_tables(self) -> None:
-        cur = self._con.cursor()
-        try:
-            create_tables(cur)
-        finally:
-            cur.close()
-
-    def drop(self) -> None:
-        cur = self._con.cursor()
-        try:
-            drop_tables(cur)
-        finally:
-            cur.close()
