97819dbd744c81c31ae73320f0fe4f1a0107f315
Rob Blackbourn <rob.blackbourn@gmail.com>
2025-04-26 13:15:51 +0100

    Added sqlite test

--- tests/impl/test_sqlite3_v2.py
+++ tests/impl/test_sqlite3_v2.py
@@ -3,7 +3,6 @@
 from contextlib import contextmanager
 from datetime import datetime, timedelta
 from decimal import Decimal
-from pathlib import Path
 import sqlite3
 from typing import Generator
 
@@ -15,22 +14,6 @@ from jetblack_pnl.impl.sqlite3_v2 import (
     Book,
     DbPnlBook
 )
-
-
-def ensure_database(database: str | Path) -> str | Path:
-    if isinstance(database, str):
-        if database == ':memory:':
-            return ':memory:'
-        database = Path(database)
-
-    if database.exists():
-        database.unlink()
-        # raise FileExistsError(f"Database exists: {database}")
-
-    if not database.parent.exists():
-        database.parent.mkdir()
-
-    return database
 
 
 @contextmanager
@@ -45,12 +28,10 @@ def cursor(con: sqlite3.Connection) -> Generator[sqlite3.Cursor, None, None]:
         cur.close()
 
 
-def main(database: str | Path):
+def test_sqlite3_v2() -> None:
     register_handlers()
 
-    database = ensure_database(database)
-
-    with sqlite3.connect(database, detect_types=sqlite3.PARSE_DECLTYPES) as con:
+    with sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES) as con:
         pnl_book = DbPnlBook()
 
         # book.drop()
@@ -65,7 +46,6 @@ def main(database: str | Path):
         with cursor(con) as cur:
             pnl = pnl_book.add_trade(apple, tech, trade, cur)
         assert pnl == (6, -600, 0)
-        print(pnl)
 
         # Buy 6 @ 106
         ts += timedelta(seconds=1)
@@ -73,7 +53,6 @@ def main(database: str | Path):
         with cursor(con) as cur:
             pnl = pnl_book.add_trade(apple, tech, trade, cur)
         assert pnl == (12, -1236, 0)
-        print(pnl)
 
         # Buy 6 @ 103
         ts += timedelta(seconds=1)
@@ -81,7 +60,6 @@ def main(database: str | Path):
         with cursor(con) as cur:
             pnl = pnl_book.add_trade(apple, tech, trade, cur)
         assert pnl == (18, -1854, 0)
-        print(pnl)
 
         # Sell 9 @ 105
         ts += timedelta(seconds=1)
@@ -89,7 +67,6 @@ def main(database: str | Path):
         with cursor(con) as cur:
             pnl = pnl_book.add_trade(apple, tech, trade, cur)
         assert pnl == (9, -936, 27)
-        print(pnl)
 
         # Sell 12 @ 107
         ts += timedelta(seconds=1)
@@ -97,7 +74,6 @@ def main(database: str | Path):
         with cursor(con) as cur:
             pnl = pnl_book.add_trade(apple, tech, trade, cur)
         assert pnl == (-3, 321, 54)
-        print(pnl)
 
         # Buy 3 @ 103
         ts += timedelta(seconds=1)
@@ -105,10 +81,3 @@ def main(database: str | Path):
         with cursor(con) as cur:
             pnl = pnl_book.add_trade(apple, tech, trade, cur)
         assert pnl == (0, 0, 66)
-        print(pnl)
-
-
-if __name__ == '__main__':
-    DATABASE = ":memory:"
-    # DATABASE = "tmp/pnl.sqlite"
-    main(DATABASE)
